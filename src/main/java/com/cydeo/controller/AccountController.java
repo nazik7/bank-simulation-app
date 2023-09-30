ab216b7782d32d0038655d17a491314c810f1be6
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-30 11:59:20 -0500

    created a method for capturing data inserted by user and showing it to user

--- src/main/java/com/cydeo/controller/AccountController.java
+++ src/main/java/com/cydeo/controller/AccountController.java
@@ -1,9 +1,15 @@
 package com.cydeo.controller;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
 import com.cydeo.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
 public class AccountController {
@@ -19,9 +25,27 @@ public class AccountController {
         return "account/index";
     }
 
-    @GetMapping("/create-account")
-    public String createAccount(Model model){
-        model.addAttribute("accountList",accountService.listAllAccount());
+    @GetMapping("/create-form")
+    public String getCreateAccount(Model model){
+        //need to provide empty account object
+        model.addAttribute("account", Account.builder().build());
+        //need to provide account type enums to view(to user)
+        model.addAttribute("accountTypes", AccountType.values());
         return "/account/create-account";
+    }
+
+    //create a method to capture information from UI
+    //print them on the console
+    ///trigger createNewAccount method, create the account based on the user input
+    //once user is created return back to index page
+
+    @PostMapping("/create")
+    public String createAccount(@ModelAttribute("account") Account account){
+        System.out.println(account);
+
+        //BigDecimal balance, Date createDate, AccountType accountType, Long userId
+        accountService.createNewAccount(account.getBalance(),new Date(),account.getAccountType(),
+        account.getUserId());
+        return "redirect:/index";
     }
 }
