d571a8402f9e757bceacdedf475825761811fb5c
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-10-28 10:59:38 -0500

    added validation for transaction

--- src/main/java/com/cydeo/controller/TransactionController.java
+++ src/main/java/com/cydeo/controller/TransactionController.java
@@ -6,11 +6,13 @@ import com.cydeo.service.AccountService;
 import com.cydeo.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -32,7 +34,12 @@ public class TransactionController {
     }
 
     @PostMapping("/transfer")
-    public String makeTransfer(@ModelAttribute("transaction")Transaction transaction){
+    public String makeTransfer(@Valid @ModelAttribute("transaction")Transaction transaction, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("accounts",accountService.listAllAccount());
+            model.addAttribute("lastTransactions", transactionService.last10Transactions());
+            return "/transaction/make-transfer";
+        }
         Account sender = accountService.retrieveById(transaction.getSender());
         Account receiver = accountService.retrieveById(transaction.getReceiver());
         transactionService.makeTransfer(sender, receiver,transaction.getAmount(), new Date(), transaction.getMessage());
