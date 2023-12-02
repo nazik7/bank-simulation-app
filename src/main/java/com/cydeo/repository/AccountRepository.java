4770f4b6f8206da2a2d38b5a1f853dd118a5f2fe
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-12-02 11:52:22 -0600

    repositories updated

--- src/main/java/com/cydeo/repository/AccountRepository.java
+++ src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,37 +1,39 @@
 package com.cydeo.repository;
 
 import com.cydeo.dto.AccountDTO;
+import com.cydeo.entity.Account;
 import com.cydeo.exception.RecordNotFoundExcdeption;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class AccountRepository {
 
-    public static List<AccountDTO> accountDTOList = new ArrayList<>();
-    public AccountDTO save(AccountDTO accountDTO){
-        accountDTOList.add(accountDTO);
-        return accountDTO;
-    }
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
-//    public void delete(UUID id){
-//        Account accountFound = accountList.stream()
-//                .filter(account -> account.getId()==id)
-//                .findFirst().get();
-//
-//        accountList.remove(accountFound);
+//    public static List<AccountDTO> accountDTOList = new ArrayList<>();
+//    public AccountDTO save(AccountDTO accountDTO){
+//        accountDTOList.add(accountDTO);
+//        return accountDTO;
 //    }
-
-    public List<AccountDTO> findAll(){
-        return accountDTOList;
-    }
-
-    public AccountDTO findById(Long id) {
-       return accountDTOList.stream()
-                .filter(account -> account.getId().equals(id))
-                .findAny()
-                .orElseThrow(()->new RecordNotFoundExcdeption("Account is not found in the database "));
-    }
+//
+////    public void delete(UUID id){
+////        Account accountFound = accountList.stream()
+////                .filter(account -> account.getId()==id)
+////                .findFirst().get();
+////
+////        accountList.remove(accountFound);
+////    }
+//
+//    public List<AccountDTO> findAll(){
+//        return accountDTOList;
+//    }
+//
+//    public AccountDTO findById(Long id) {
+//       return accountDTOList.stream()
+//                .filter(account -> account.getId().equals(id))
+//                .findAny()
+//                .orElseThrow(()->new RecordNotFoundExcdeption("Account is not found in the database "));
+//    }
 }
--- src/main/java/com/cydeo/repository/TransactionRepository.java
+++ src/main/java/com/cydeo/repository/TransactionRepository.java
@@ -1,6 +1,8 @@
 package com.cydeo.repository;
 
 import com.cydeo.dto.TransactionDTO;
+import com.cydeo.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,32 +10,32 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
-public class TransactionRepository {
 
-    public static List<TransactionDTO> transactionDTOList = new ArrayList<>();
-    public TransactionDTO save(TransactionDTO transactionDTO){
-        transactionDTOList.add(transactionDTO);
-        return transactionDTO;
-    }
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-
-    public List<TransactionDTO> findAll() {
-        return transactionDTOList;
-    }
-
-    public List<TransactionDTO> findLast10Transactions(){
-        return findAll().stream()
-                .sorted(Comparator.comparing(TransactionDTO::getCreateDate).reversed())
-                .limit(10)
-                .collect(Collectors.toList());
-    }
-
-    public List<TransactionDTO> findTransactionsById(Long id){
-
-        return findAll().stream()
-                .filter(transactionDTO -> transactionDTO.getReceiver().equals(id) ||
-                        transactionDTO.getSender().equals(id))
-                .collect(Collectors.toList());
-    }
+//    public static List<TransactionDTO> transactionDTOList = new ArrayList<>();
+//    public TransactionDTO save(TransactionDTO transactionDTO){
+//        transactionDTOList.add(transactionDTO);
+//        return transactionDTO;
+//    }
+//
+//
+//    public List<TransactionDTO> findAll() {
+//        return transactionDTOList;
+//    }
+//
+//    public List<TransactionDTO> findLast10Transactions(){
+//        return findAll().stream()
+//                .sorted(Comparator.comparing(TransactionDTO::getCreateDate).reversed())
+//                .limit(10)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public List<TransactionDTO> findTransactionsById(Long id){
+//
+//        return findAll().stream()
+//                .filter(transactionDTO -> transactionDTO.getReceiver().equals(id) ||
+//                        transactionDTO.getSender().equals(id))
+//                .collect(Collectors.toList());
+//    }
 }
