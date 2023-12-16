4259691820971cdcc9f12545388a54cf3fa85fa4
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-12-16 10:40:16 -0600

    added User, Role, UserRepository and Security classes

---
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class SecurityServiceImpl implements SecurityService {
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if(user ==null){
+            throw new UsernameNotFoundException("This user does not exist");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
